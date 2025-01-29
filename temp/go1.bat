9a4786b641e851abdef5b8880039bfa3ef44ed83
n3684777 <38023267+n3684777@users.noreply.github.com>
2025-01-29 11:50:01 +0800

    全部完整檔案

---
+++ temp/go1.bat
@@ -0,0 +1,4 @@
+rmdir RoEP21Server /s /q
+git clone https://github.com/n3684777/RoEP21Server.git
+xcopy C:\Users\mason\OneDrive\орн▒\RoEP21Server\temp\RoEP21Server C:\Users\mason\OneDrive\орн▒\RoEP21Server /S /E /Y
+pause
